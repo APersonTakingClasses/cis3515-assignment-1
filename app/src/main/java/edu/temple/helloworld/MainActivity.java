551c7774f4834c90343ad1f386b3a2821a579333
caffine <34240605+caffinecravings@users.noreply.github.com>
2021-02-11 08:41:16 -0500

    checking it uploaded on git before lab

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,28 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import andriod.widget.Button;
+import andriod.widget.TextView;
+import andriod.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textV);
+        textV.setText("Click This");
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                    textV.setText("I have been clicked!");
+
+            }
+        });
     }
 }
